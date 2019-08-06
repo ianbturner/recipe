f166a7a82410194328b3ea3bc6f1291875e76f9d
Ian Turner <ian.turner@arris.com>
2019-08-06 15:48:55 +0100

    Added initialise data for the database using Spring Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO category (description) VALUES( 'American' );
+INSERT INTO category (description) VALUES( 'Italian' );
+INSERT INTO category (description) VALUES( 'Mexican' );
+INSERT INTO category (description) VALUES( 'Fast Food' );
+INSERT INTO Unit_Of_Measure (description) VALUES( 'Teaspoon' );
+INSERT INTO Unit_Of_Measure (description) VALUES( 'Tablespoon' );
+INSERT INTO Unit_Of_Measure (description) VALUES( 'Cup' );
+INSERT INTO Unit_Of_Measure (description) VALUES( 'Pinch' );
+INSERT INTO Unit_Of_Measure (description) VALUES( 'Ounce' );
+INSERT INTO Unit_Of_Measure (description) VALUES( 'Gram' );
+
+
+
